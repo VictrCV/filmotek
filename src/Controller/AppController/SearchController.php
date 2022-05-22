f00bf5b8b94dee2cff1090f4db70fa7acdb9e437
Victor Claramunt <victorclaramuntv@gmail.com>
2022-05-23 00:51:59 +0200

    add filters and show results #6

--- src/Controller/AppController/SearchController.php
+++ src/Controller/AppController/SearchController.php
@@ -8,12 +8,23 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 /**
  * @codeCoverageIgnore
  */
 class SearchController extends AbstractController
 {
+    public const BASE_URL = 'https://moviesdatabase.p.rapidapi.com';
+    public const RAPIDAPI_HOST = 'moviesdatabase.p.rapidapi.com';
+    public const RAPIDAPI_KEY = '6eba30a07dmsh958077cd42aab13p17bf28jsnf44b61beabc9';
+
+    private HttpClientInterface $client;
+
+    public function __construct(HttpClientInterface $client)
+    {
+        $this->client = $client;
+    }
 
     /**
      * @Route("/", name="search")
@@ -26,6 +37,41 @@ class SearchController extends AbstractController
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $formData = $form->getData();
+            $title = $formData['title'];
+            $exact = $formData['exact'] ? 'true' : null;
+            $genre = $formData['genre'];
+            $type = $formData['type'];
+
+            $url = self::BASE_URL . '/titles';
+            if (isset($title))
+                $url .= '/search/title/' . rawurlencode($title);
+
+            $response = $this->client->request(
+                'GET',
+                $url,
+                [
+                    'headers' => [
+                        'X-RapidAPI-Host' => self::RAPIDAPI_HOST,
+                        'X-RapidAPI-Key' => self::RAPIDAPI_KEY
+                    ],
+                    'query' => [
+                        'info' => 'base_info',
+                        'limit' => 50,
+                        'exact' => $exact,
+                        'titleType' => $type,
+                        'genre' => $genre,
+                    ]
+                ]
+            );
+
+            if ($response->getStatusCode() == Response::HTTP_OK) {
+                $results = $response->toArray()['results'];
+                return $this->render('search/results.html.twig', [
+                    'results' => $results,
+                ]);
+            } else {
+                $this->addFlash('error', 'Oops! Something went wrong and the search could not be done.');
+            }
         }
 
         return $this->render('search/search.html.twig', [
--- src/Form/SearchFormType.php
+++ src/Form/SearchFormType.php
@@ -2,17 +2,23 @@
 
 namespace App\Form;
 
+use App\Controller\AppController\SearchController;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Exception\TransformationFailedException;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 /**
  * @codeCoverageIgnore
  */
-class SearchFormType extends AbstractType
+class SearchFormType extends AbstractType implements EventSubscriberInterface
 {
     private HttpClientInterface $client;
 
@@ -26,34 +32,69 @@ class SearchFormType extends AbstractType
         $builder
             ->add('title', TextType::class, [
                 'label' => false,
+                'required' => false,
                 'attr' => [
                     'placeholder' => 'Title',
                 ]
             ])
-            ->add('genres', ChoiceType::class, [
-                'choices' => [
-                    'Genre' => $this->getGenres(),
-                ],
+            ->add('exact', CheckboxType::class, [
+                'label' => 'Exact title',
+                'required' => false
+            ])
+            ->add('genre', ChoiceType::class, [
+                'choices' => $this->getGenres(),
                 'label' => false,
-                'multiple' => true,
+                'required' => false,
+                'placeholder' => 'Genre'
+            ])
+            ->add('type', ChoiceType::class, [
+                'choices' => [
+                    'TV Series' => 'tvSeries',
+                    'Movie' => 'movie',
+                    'TV Mini-Series' => 'tvMiniSeries',
+                    'TV Movie' => 'tvMovie'],
+                'label' => false,
+                'required' => true
             ])
             ->add('search', SubmitType::class, [
                 'label' => 'Search',
                 'attr' => [
                     'class' => 'btn-sign-up-form'
                 ],
-            ]);
+            ])
+            ->addEventSubscriber($this);
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            FormEvents::SUBMIT => 'ensureOneFieldIsSubmitted',
+        ];
+    }
+
+    public function ensureOneFieldIsSubmitted(FormEvent $event)
+    {
+        $submittedData = $event->getData();
+
+        if (!isset($submittedData['title']) && !isset($submittedData['genre'])) {
+            throw new TransformationFailedException(
+                'Set title and/or genre',
+                0,
+                null,
+                'Set title and/or genre.'
+            );
+        }
     }
 
     private function getGenres(): array
     {
         $response = $this->client->request(
             'GET',
-            'https://data-imdb1.p.rapidapi.com/titles/utils/genres',
+            SearchController::BASE_URL . '/titles/utils/genres',
             [
                 'headers' => [
-                    'X-RapidAPI-Host' => 'data-imdb1.p.rapidapi.com',
-                    'X-RapidAPI-Key' => '6eba30a07dmsh958077cd42aab13p17bf28jsnf44b61beabc9'
+                    'X-RapidAPI-Host' => SearchController::RAPIDAPI_HOST,
+                    'X-RapidAPI-Key' => SearchController::RAPIDAPI_KEY
                 ]
             ]
         );
