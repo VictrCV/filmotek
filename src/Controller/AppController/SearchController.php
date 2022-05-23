1f456ac83a5e8e3477ac3624fea3da26fbe6b43e
Victor Claramunt <victorclaramuntv@gmail.com>
2022-05-23 02:46:31 +0200

    add details #6

--- src/Controller/AppController/SearchController.php
+++ src/Controller/AppController/SearchController.php
@@ -78,4 +78,37 @@ class SearchController extends AbstractController
             'form' => $form->createView(),
         ]);
     }
+
+    /**
+     * @Route("/search/details/{apiId}", name="search_details")
+     * @param string $apiId
+     * @return RedirectResponse|Response
+     */
+    public function details(string $apiId): RedirectResponse|Response
+    {
+
+        $response = $this->client->request(
+            'GET',
+            self::BASE_URL . '/titles/' . $apiId,
+            [
+                'headers' => [
+                    'X-RapidAPI-Host' => self::RAPIDAPI_HOST,
+                    'X-RapidAPI-Key' => self::RAPIDAPI_KEY
+                ],
+                'query' => [
+                    'info' => 'base_info'
+                ]
+            ]
+        );
+
+        if ($response->getStatusCode() == Response::HTTP_OK) {
+            $details = $response->toArray()['results'];
+            return $this->render('search/details.html.twig', [
+                'details' => $details,
+            ]);
+        } else {
+            $this->addFlash('error', 'Oops! Something went wrong and the details could not be loaded.');
+        }
+        return $this->redirectToRoute('search', []);
+    }
 }
