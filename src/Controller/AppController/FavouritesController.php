452205a558cda79d9b0ebd216c45edae049b4971
Victor Claramunt <victorclaramuntv@gmail.com>
2022-05-31 01:07:15 +0200

    ignore FavouritesController in coverage report #10

--- src/Controller/AppController/FavouritesController.php
+++ src/Controller/AppController/FavouritesController.php
@@ -11,6 +11,9 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @codeCoverageIgnore
+ */
 class FavouritesController extends AbstractController
 {
     private SeriesListApiController $seriesListApiController;
