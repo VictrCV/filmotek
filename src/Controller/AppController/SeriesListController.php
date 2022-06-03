b89375ef7f90fc4d7ea8fb8c02d6f91ddefa6286
Victor Claramunt <victorclaramuntv@gmail.com>
2022-06-03 14:00:38 +0200

    add remove series from list button #13

--- src/Controller/AppController/SeriesListController.php
+++ src/Controller/AppController/SeriesListController.php
@@ -129,63 +129,61 @@ class SeriesListController extends SeriesController
     }
 
     /**
-     * @Route("{list}/series_list/start_watching/{apiId}", name="series_list_start_watching")
+     * @Route("{list}/series_list/start_watching/{apiId}/{seriesListId}", name="series_list_start_watching")
      * @param Request $request
      * @param string $list
      * @param string $apiId
+     * @param int $seriesListId
      * @return RedirectResponse|Response
      */
-    public function startWatching(Request $request, string $list, string $apiId): RedirectResponse|Response
+    public function startWatching(Request $request, string $list, string $apiId, int $seriesListId): RedirectResponse|Response
     {
-        $errorMessage = 'Oops! Something went wrong and it was not possible to start watching the series.';
-        $session = $request->getSession();
-        $userId = $session->get(UserApiController::USER_ID);
-
         $request = Request::create(
-            SeriesApiController::SERIES_GET_BY_API_ID_ROUTE . $apiId
+            SeriesListApiController::SERIES_LIST_API_ROUTE . '/' . $seriesListId,
+            'PUT',
+            [], [], [], [],
+            json_encode([SeriesList::TYPE_ATTR => SeriesList::IN_PROGRESS])
         );
-        $response = $this->seriesApiController->getByApiIdAction($request, $apiId);
+        $response = $this->seriesListApiController->putAction($request, $seriesListId);
 
-        if ($response->getStatusCode() == Response::HTTP_OK) {
-            $seriesId = json_decode($response->getContent(), true)[Series::SERIES_ATTR]['id'];
-
-            $data = [
-                SeriesList::TYPE_ATTR => SeriesList::TO_WATCH,
-                SeriesList::SERIES_ATTR => $seriesId
-            ];
-
-            $request = Request::create(
-                SeriesListApiController::SERIES_LIST_GET_BY_USER_ROUTE . $userId,
-                'GET',
-                $data,
-
-            );
-            $response = $this->seriesListApiController->getByUserAction($request, $userId);
-
-            if ($response->getStatusCode() == Response::HTTP_OK) {
-                $seriesListId = json_decode($response->getContent(), true)[SeriesList::SERIES_LIST_ATTR][0]['id'];
-
-                $request = Request::create(
-                    SeriesListApiController::SERIES_LIST_API_ROUTE . '/' . $seriesListId,
-                    'PUT',
-                    [], [], [], [],
-                    json_encode([SeriesList::TYPE_ATTR => SeriesList::IN_PROGRESS])
-                );
-                $response = $this->seriesListApiController->putAction($request, $seriesListId);
-
-                if ($response->getStatusCode() == Response::HTTP_OK) {
-                    return $this->redirectToRoute('series', [
-                        'list' => 'in_progress',
-                        Series::API_ID_ATTR => $apiId
-                    ]);
-                }
-            }
+        if ($response->getStatusCode() != Response::HTTP_OK) {
+            $this->addFlash('error', 'Oops! Something went wrong and it was not possible to start watching the series.');
+            return $this->redirectToRoute('series', [
+                'list' => $list,
+                Series::API_ID_ATTR => $apiId
+            ]);
         }
 
-        $this->addFlash('error', $errorMessage);
         return $this->redirectToRoute('series', [
-            'list' => $list,
+            'list' => 'in_progress',
             Series::API_ID_ATTR => $apiId
         ]);
+    }
+
+    /**
+     * @Route("{list}/series_list/delete/{apiId}/{seriesListId}", name="series_list_delete")
+     * @param Request $request
+     * @param string $list
+     * @param string $apiId
+     * @param int $seriesListId
+     * @return RedirectResponse|Response
+     */
+    public function deleteSeriesList(Request $request, string $list, string $apiId, int $seriesListId): RedirectResponse|Response
+    {
+        $request = Request::create(
+            SeriesListApiController::SERIES_LIST_API_ROUTE . '/' . $seriesListId,
+            'DELETE'
+        );
+        $response = $this->seriesListApiController->deleteAction($request, $seriesListId);
+
+        if ($response->getStatusCode() != Response::HTTP_NO_CONTENT) {
+            $this->addFlash('error', 'Oops! Something went wrong and it was not possible to start watching the series.');
+            return $this->redirectToRoute('series', [
+                'list' => $list,
+                Series::API_ID_ATTR => $apiId
+            ]);
+        }
+
+        return $this->redirectToRoute($list);
     }
 }
