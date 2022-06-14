90b7aa56b75b2050a567bee201bee0affaa78d51
Victor Claramunt <victorclaramuntv@gmail.com>
2022-06-14 18:19:12 +0200

    add comment form #19

---
+++ src/Form/CommentType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+/**
+ * @codeCoverageIgnore
+ */
+class CommentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('text', TextType::class, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Add a comment...',
+                ]
+            ])
+            ->add('comment', SubmitType::class, [
+                'label' => 'Comment',
+                'attr' => [
+                    'class' => 'btn-form'
+                ],
+            ]);
+    }
+}
